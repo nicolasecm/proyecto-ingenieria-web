daf773a2f2be33441b1bda292b9fd0a1132ff79c
nicolasecm <nicolasecm@hotmail.com>
2021-10-13 15:01:50 -0500

    Se agrego la validacion a los campos

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,5 @@ class Company < ApplicationRecord
   belongs_to :category
   belongs_to :payment_method
   validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- app/models/payment_method.rb
+++ app/models/payment_method.rb
@@ -1,3 +1,5 @@
 class PaymentMethod < ApplicationRecord
     has_many :company
+    validates :name, presence: true
+    validates :name, uniqueness: true
 end
